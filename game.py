388d3ecc1b26a43aec884ad2ea0f7f064e1cb32c
abylsliam44 <160605719+abylsliam44@users.noreply.github.com>
2024-02-22 09:10:44 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,9 @@
 import pygame
 import random
 
-# Инициализация Pygame
 pygame.init()
 pygame.mixer.init()
 
-# Определение размеров экрана
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Spaceship")
